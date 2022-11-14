56759391df9b8c9d0205d71ea35e9728dd7d41a3
ilan <ilan@cycode.com>
2022-11-14 13:37:27 +0200

    Revert "init"
    This reverts commit e7d402dc6fbe32b9de38492e0474ef0596992199.

--- providers/provider_handler.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-
-class ProviderHandler(ABC):
-    @abstractmethod
-    def release_branch_protection(self, token, organization_name, repository_name, branch, contexts: List[str]):
-        pass
