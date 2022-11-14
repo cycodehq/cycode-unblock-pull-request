8e3a649e620bccf3c049f7d6048720fe463c9163
ilan <ilan@cycode.com>
2022-11-14 12:44:01 +0200

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+from typing import Dict
+
+from PyInquirer import prompt, Separator
+import logging
+from consts import providers, contexts, config_file
+from menus.menu_base import MenuBase
+from menus.release_block_pr_menu import ReleaseBlockPrMenu
+
+release_block_pr_menu = ReleaseBlockPrMenu()
+menus = [release_block_pr_menu]
+menu_value_to_class: Dict[str, MenuBase] = {menu.get_menu(): menu for menu in menus}
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+main_menu = [
+    {
+        'type': 'list',
+        'name': 'menu',
+        'message': 'What do you want to do?',
+        'choices': [menu for menu in menu_value_to_class.keys()],
+
+    },
+    {
+        'type': 'input',
+        'name': 'config_file',
+        "message": "Please provide a configuration file",
+        "default": config_file,
+        'validate': release_block_pr_menu.validate_config_file,
+        "when": release_block_pr_menu.menu_chosen
+    },
+    {
+        'type': 'list',
+        'name': 'provider',
+        "message": "Which provider?",
+        "choices": providers,
+        "when": release_block_pr_menu.menu_chosen,
+    },
+    {
+        'type': 'checkbox',
+        'name': 'contexts',
+        "message": "Which contexts?",
+        "choices": [{"name": context} for context in contexts],
+        "when": release_block_pr_menu.menu_chosen
+    }
+]
+
+if __name__ == '__main__':
+    answer = prompt(main_menu)
+    menu = menu_value_to_class.get(answer.get('menu'))
+    if menu:
+        menu.handle(answer)
+    else:
+        logger.error(f"invalid menu {answer.get('menu')}")
